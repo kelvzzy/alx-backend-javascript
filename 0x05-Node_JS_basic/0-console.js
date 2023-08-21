107d4a4925aac51d7a81b0831a3cfc41890914f9
kelvzzy <kelvinworldstudy@yahoo.com>
2023-08-21 17:51:37 +0000

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message on the STDOUT.
+	* @param {Strings} msg The message to display.
+	* @author Kelvin Uche <https://github.com/kelvzzy>
+	*/
+const displayMessage = (msg) => {console.log(msg);
+};
+
+module.exports =  displayMessage;
